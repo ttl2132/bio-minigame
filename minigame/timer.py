460adca71a7ab07faa76c19d19efe7de8ef8c251
chiyo <ttl2132@columbia.edu>
2021-03-07 20:39:54 -0500

    Add more timer info

--- minigame/timer.py
+++ minigame/timer.py
@@ -3,4 +3,8 @@ from kivy.clock import Clock
 
 class Timer():
     """Used to dynamically display the current time elapsed on the screen."""
-    pass
+    def update_time(self):
+        """Updates the time elapsed."""
+
+    def reset_time(self):
+        """Resets the time elapsed."""
