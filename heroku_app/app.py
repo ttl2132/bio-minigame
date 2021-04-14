d0e141458fd84671d3784416e28f7c13871b7c70
chiyo <ttl2132@columbia.edu>
2021-04-14 06:24:24 -0400

    Adding push and token to app

--- heroku_app/app.py
+++ heroku_app/app.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import uvicorn
 import os
-from github import Github
+from github import Github, InputGitAuthor
 from fastapi import FastAPI
 from loguru import logger
 
@@ -26,23 +26,40 @@ def update_scores(initials: str, score: str, rank: int):
         db.iloc[i] = db.iloc[i-1]
     db.iloc[rank][0] = initials
     db.iloc[rank][1] = score
-    with open(f"{LB_URL}/leaderboard.csv", 'w') as f:
-        f.write(db.to_csv())
+    push(db.to_csv())
     return "posted"
 
 @app.get("/scores")
 def get_scores():
-    token = os.getenv('TOKEN')
-    logger.debug('TOKEN')
-    g = Github(token)
-    repo = g.get_repo("ttl2132/bio-minigame")
-    issues = repo.get_issues(state="closed")
-    logger.debug(issues.get_page(0))
     "Gets the data from the URL and returns the information as a JSON."
     data = pd.read_csv(f"{LB_URL}/leaderboard.csv")
     data = data.fillna("N/A")
     logger.debug(data)
     return data
+
+def push(content, update=True):
+    """From https://towardsdatascience.com/all-the-things-you-can-do-with-github-api-and-python-f01790fca131"""
+    token = os.getenv('TOKEN')
+    path = "/data/leaderboard.csv"
+    g = Github(token)
+    repo = g.get_repo("ttl2132/ttl2132.github.io")
+    branch = "master"
+
+    message = "Updating github data"
+    file = repo.get_contents(path, ref="master")  # Get file from branch
+    data = file.decoded_content.decode("utf-8")  # Get raw string data
+    data += "\npytest==5.3.2"  # Modify/Create file
+    author = InputGitAuthor(
+        "ttl2132",
+        "ttl2132@columbia.edu"
+    )
+    source = repo.get_branch("master")
+    repo.create_git_ref(ref=f"refs/heads/{branch}", sha=source.commit.sha)  # Create new branch from master
+    if update:  # If file already exists, update it
+        contents = repo.get_contents(path, ref=branch)  # Retrieve old file to get its SHA and path
+        repo.update_file(contents.path, message, content, contents.sha, branch=branch, author=author)  # Add, commit and push branch
+    else:  # If file doesn't exist, create it
+        repo.create_file(path, message, content, branch=branch, author=author)  # Add, commit and push branch
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
